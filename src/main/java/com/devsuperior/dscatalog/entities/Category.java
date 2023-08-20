ee7e593e33a3e3c0d5439446caa67f76f628580d
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-08-19 22:37:07 -0300

    feat:input layers

--- src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,12 +1,18 @@
 package com.devsuperior.dscatalog.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
 
