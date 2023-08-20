6fc2e206e5710930d120b49de816a890ac0f4140
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-08-19 21:03:39 -0300

    Initial commit

---
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.Entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Category implements Serializable {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return getId().equals(category.getId()) && getName().equals(category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+}
